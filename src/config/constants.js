14b902884cc53194b71fcbe8546726a2cf636861
LIU Zhao <liuzhaomax@163.com>
2022-08-13 18:20:31 +1000

    update contributors

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,68 +1,26 @@
-export const GITHUB = "https://github.com/liuzhaomax?tab=repositories"
+export const GITHUB = "https://github.com/liuzhaomax"
 
 export const CONTRIBUTORS = [
 	{
 		ID: 1,
-		NAME: "LIU Zhao",
+		NAME: "liuzhaomax",
 		JOB: "架构|DevOps|全栈",
 		LINK: GITHUB,
 		AVATAR:"https://avatars.githubusercontent.com/u/20788414?v=4",
 	},
 	{
 		ID: 2,
-		NAME: "LIU Zhao",
-		JOB: "架构|DevOps|全栈",
-		LINK: GITHUB,
-		AVATAR:"https://avatars.githubusercontent.com/u/20788414?v=4",
+		NAME: "csht2010",
+		JOB: "前端",
+		LINK: "https://github.com/csht2010",
+		AVATAR:"https://avatars.githubusercontent.com/u/74046743?v=4",
 	},
 	{
 		ID: 3,
-		NAME: "LIU Zhao",
-		JOB: "架构|DevOps|全栈",
-		LINK: GITHUB,
-		AVATAR:"https://avatars.githubusercontent.com/u/20788414?v=4",
-	},
-	{
-		ID: 4,
-		NAME: "LIU Zhao",
-		JOB: "架构|DevOps|全栈",
-		LINK: GITHUB,
-		AVATAR:"https://avatars.githubusercontent.com/u/20788414?v=4",
-	},
-	{
-		ID: 5,
-		NAME: "LIU Zhao",
-		JOB: "架构|DevOps|全栈",
-		LINK: GITHUB,
-		AVATAR:"https://avatars.githubusercontent.com/u/20788414?v=4",
-	},
-	{
-		ID: 6,
-		NAME: "LIU Zhao",
-		JOB: "架构|DevOps|全栈",
-		LINK: GITHUB,
-		AVATAR:"https://avatars.githubusercontent.com/u/20788414?v=4",
-	},
-	{
-		ID: 7,
-		NAME: "LIU Zhao",
-		JOB: "架构|DevOps|全栈",
-		LINK: GITHUB,
-		AVATAR:"https://avatars.githubusercontent.com/u/20788414?v=4",
-	},
-	{
-		ID: 8,
-		NAME: "LIU Zhao",
-		JOB: "架构|DevOps|全栈",
-		LINK: GITHUB,
-		AVATAR:"https://avatars.githubusercontent.com/u/20788414?v=4",
-	},
-	{
-		ID: 9,
-		NAME: "LIU Zhao",
-		JOB: "架构|DevOps|全栈",
-		LINK: GITHUB,
-		AVATAR:"https://avatars.githubusercontent.com/u/20788414?v=4",
+		NAME: "zackzzy",
+		JOB: "前端",
+		LINK: "https://github.com/zackzzy",
+		AVATAR:"https://avatars.githubusercontent.com/u/89061751?v=4",
 	}
 ]
 
