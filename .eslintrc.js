126ff4fa5cca0e84c00467efdc6075bc3c48c738
LIU Zhao <liuzhaomax@163.com>
2022-08-29 00:22:55 +1000

    github pr check action (#39)
    * github pr check action
    
    * github pr check action

--- .eslintrc.js
+++ .eslintrc.js
@@ -28,7 +28,9 @@ module.exports = {
         "server",
         "scripts",
         "test",
-        "jestGlobalSetup.js"
+        "jestGlobalSetup.js",
+        "sonar-scanner.properties",
+        "pipeline/**/*"
     ],
     "rules": {
         "indent": [
