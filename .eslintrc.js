33f834f5d008d0fe80ff7085d49649e821414c72
LIU Zhao <liuzhaomax@163.com>
2022-08-29 05:36:07 +1000

    CRLF (#41)
    * CRLF
    
    * CRLF
    
    * CRLF
    
    * CRLF

--- .eslintrc.js
+++ .eslintrc.js
@@ -37,10 +37,7 @@ module.exports = {
             "error",
             4
         ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
+        "linebreak-style": "off",
         "quotes": [
             "error",
             "double"
