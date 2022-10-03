49a97dc0a20328d7aa77836eed53721627f7bf45
LIU Zhao <liuzhaomax@163.com>
2022-10-04 01:14:11 +1100

    project layout home layout

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,17 +1,29 @@
-import React from "react"
+import React, { useEffect } from "react"
 import "./Home.css"
 import { HOME } from "../../config/cstModule"
-// import HomeProjIntro from "./HomeProjIntro"
+import HomeProjIntro from "./HomeProjIntro"
 
 function Home() {
-
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+            let test = document.getElementById("home-test")
+            let value = window.scrollY
+            console.log(value)
+            if (value >= 1695) {
+                test.classList.remove("home-test-relative")
+                test.classList.add("home-test-fixed")
+            } else {
+                test.classList.remove("home-test-fixed")
+                test.classList.add("home-test-relative")
+            }
+        })
+    },[])
     return (
         <div id={HOME.KEY} className={HOME.KEY}>
-            {/*<HomeProjIntro/>*/}
-            <div className="home-test">I m home1</div>
-            <div className="home-test">I m home2</div>
-            <div className="home-test">I m home3</div>
-            <div className="home-test">I m home4</div>
+            <HomeProjIntro/>
+            <div id="home-test" className="home-test home-test-relative">
+                sdfsdf
+            </div>
         </div>
     )
 }
