0dba992b945ed745c4a2209144b8cefee95877d8
Ziyu Zhang <zzha0207@student.monash.edu>
2022-10-12 18:58:27 +1100

    demo加载更多 防抖

--- src/pages/demo/Demo.jsx
+++ src/pages/demo/Demo.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react"
+import React, { useEffect } from "react"
 import "./Demo.css"
 import "./DemoNorth.css"
 import { DEMO } from "../../config/cstModule"
@@ -10,10 +10,10 @@ import ReactDOM from "react-dom/client"
 import { useNavigate } from "react-router-dom"
 
 let data = []
+let count = 0
 
 function Demo() {
     const navigate = useNavigate()
-    const [count, setCount] = useState(0)
     let root
 
     useEffect(() => {
@@ -22,6 +22,7 @@ function Demo() {
                 .then(res => {
                     loadCards(res.data.data)
                     data = data.concat(res.data.data)
+                    count = count + 1
                 })
                 .catch(err => {
                     console.log((err))
@@ -29,8 +30,11 @@ function Demo() {
         })()
         let container = document.getElementById("demo-card-wrap")
         root = ReactDOM.createRoot(container)
-        scrollToShowMore()
     },[])
+
+    useEffect(() => {
+        scrollToShowMore()
+    },[count])
 
     const loadCards = demos => {
         if(data !== null) {
@@ -46,25 +50,74 @@ function Demo() {
     }
 
     const scrollToShowMore = () => {
-        let more = document.getElementById("demo-card-more-card")
-        let footer = document.getElementsByClassName("Footer")[0]
-        let body = document.getElementById("App")
-        window.addEventListener("scroll", () => {
+        window.addEventListener("scroll", (e) => {
+            let more = document.getElementById("demo-card-more-card")
+            let footer = document.getElementsByClassName("Footer")[0]
+            let body = document.getElementById("App")
             // more顶部到网页顶部的距离 - 窗口的高度 => more在窗口出现的时机 = 将要滚动到more时滚动的距离
-            if (window.scrollY >= body.clientHeight - footer.clientHeight - more.clientHeight - window.innerHeight ) {
-                loadMoreCards()
+            if (window.scrollY >= body.clientHeight - footer.clientHeight - more.clientHeight - window.innerHeight) {
+                debounceLoadMoreCards(e)
             }
         })
     }
 
     const loadMoreCards = () => {
-        setCount(count+1)
         if(count < 3){
             (async () => {
-                await getDemoByCount(count + 1)
+                await getDemoByCount(count)
                     .then(res => {
                         loadCards(res.data.data)
                         data = data.concat(res.data.data)
+                        count = count + 1
+                    })
+                    .catch(err => {
+                        console.log(err)
+                    })
+            })()
+        }
+    }
+
+    // function throttle(fun, delay) {
+    //     let last, deferTimer
+    //     return function (args) {
+    //         let that = this
+    //         let _args = args
+    //         let now = +new Date()
+    //         if (last && now < last + delay) {
+    //             clearTimeout(deferTimer)
+    //             deferTimer = setTimeout(function () {
+    //                 last = now
+    //                 fun.apply(that, _args)
+    //             }, delay)
+    //         }else {
+    //             last = now
+    //             fun.apply(that,_args)
+    //         }
+    //     }
+    // }
+
+    function debounce(fun, delay) {
+        return function (args) {
+            let that = this
+            let _args = args
+            clearTimeout(fun.id)
+            fun.id = setTimeout(function () {
+                fun.call(that, _args)
+            }, delay)
+        }
+    }
+
+    // let throttleLoadMoreCards = throttle(loadMoreCards, 2000)
+    let debounceLoadMoreCards = debounce(loadMoreCards, 200)
+
+    const clickToLoadMoreCards = () => {
+        if(count >= 3) {
+            (async () => {
+                await getDemoByCount(count)
+                    .then(res => {
+                        loadCards(res.data.data)
+                        data = data.concat(res.data.data)
+                        count = count + 1
                     })
                     .catch(err => {
                         console.log(err)
@@ -85,7 +138,7 @@ function Demo() {
                 <div className="demo-south">
                     <DemoSearch/>
                     <div id="demo-card-wrap" className="demo-card-wrap"/>
-                    <div id="demo-card-more-card" className="demo-card-more-card">
+                    <div id="demo-card-more-card" className="demo-card-more-card" onClick={clickToLoadMoreCards}>
                         ↓更多样例↓
                     </div>
                 </div>
